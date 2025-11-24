3448b1dbb0b8b1e691233e8368d37c6414f1a958
coderalitutorials <razamuneer670@gmail.com>
2025-11-24 05:12:01 -0800

    Fix CORS for serverless functions + Netlify

--- app.js
+++ app.js
@@ -13,11 +13,28 @@ app.use(express.urlencoded({ extended: true }));
 
 
   
+const allowedOrigins = [
+  "http://localhost:5173",                   // local dev
+  "https://candid-ganache-4af8e6.netlify.app"  // deployed frontend
+];
 
-app.use(cors({
- origin:"https://candid-ganache-4af8e6.netlify.app",
-  credentials: true
-}));
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  
+  // Handle preflight request
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 
 
 
