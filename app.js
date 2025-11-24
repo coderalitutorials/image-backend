752497b7e0734e41cc9531288f7c9e6f6e924670
coderalitutorials <razamuneer670@gmail.com>
2025-11-24 05:00:47 -0800

    Update app.js CORS setup

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const allowedOrigins = [
-  "https://candid-ganache-4af8e6.netlify.app/"
-]
+
+  
+
 app.use(cors({
-  // origin: "http://localhost:5173",
-  origin:allowedOrigins,
+ 
+  origin:"https://candid-ganache-4af8e6.netlify.app/",
   credentials: true
 }));
 
